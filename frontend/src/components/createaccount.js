fd54c43799ded19b984863c1a99a3bb9653dc043
danthb <danthb@hotmail.com>
2022-01-08 18:11:48 -0500

    Added signUp firebase

--- frontend/src/components/createaccount.js
+++ frontend/src/components/createaccount.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { AuthContext } from "../contexts/Auth/AuthProvider";
+import { accountAPI } from "../services";
 import BankForm from "./bankform";
 
 export default function CreateAccount() {
 
-    const auth = React.useContext(AuthContext);  
+    async function handle(newUser){
+      console.log(newUser);
+      const { name, email, password } = newUser;
+      try {
+        const { status, data } = await accountAPI.createUser(newUser);
+        console.log(status, data);
+        if (status === 200) {
+          console.log('user created');
+        }
+      } catch (error) {
+        console.log(error);
 
-    function handle(newUser){
-      auth.users.push({name: newUser.name, email: newUser.email, password: newUser.password, balance: 100, isLoged: false
-      })
-      console.log()
+      }
       return true
     }
     return (
