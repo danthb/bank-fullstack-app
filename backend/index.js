3a739558f071fa1e0656460365691a9b1df562bf
danthb <danthb@hotmail.com>
2022-01-09 12:14:03 -0500

    Updated schema fields and added middleware for decode token

--- backend/index.js
+++ backend/index.js
@@ -2,29 +2,31 @@ require('dotenv').config();
 const express = require('express');
 const app     = express();
 const cors    = require('cors');
-const e = require('express');
 const User = require('./models/User')
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const firebase = require('./config/firebase.js');
+const {decodeToken} = require('./middlewares/authFB');
 const corsOptions = {
     origin: process.env.CLIENT_URL,
 }
-const mongoose = require('mongoose');
-const firebase = require('./config/firebase.js');
 
-// connect to mongo
+// connect to mongo database
 mongoose
     .connect(process.env.DB_URL,
         { useNewUrlParser: true, useUnifiedTopology: true })
     .then((x) => console.log(`Connected to MongoDB, database: ${x.connections[0].name}`))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
+// middlewares
 // used to serve static files from public directory
 app.use(express.static('public'));
 app.use(bodyParser.json())
 app.use(cors(corsOptions));
 
-//Routes
 
+
+//Routes
 // get all users
 app.get('/account/all', function (req, res) {
     User.find({}, function (err, users) {
@@ -48,7 +50,7 @@ app.get('/account/:id', function (req, res) {
 
 // create user account
 app.post('/account/create', async(req, res) => {
-    const { username, email, password} = req.body;
+    const { name, email, password} = req.body;
     console.log(req.body);
     try {
         const firebaseUser = await firebase.auth().createUser({
@@ -58,7 +60,7 @@ app.post('/account/create', async(req, res) => {
             disabled: false
         })
         console.log('firebaseUser', firebaseUser)
-        const dbUser = await User.create({username, email, firebaseId: firebaseUser.uid, balance: 0})
+        const dbUser = await User.create({name, email, firebaseId: firebaseUser.uid, balance: 0})
         console.log('dbUser', dbUser)
     } catch (error) {
         console.log(error)
